6ec2972a0499a4acf85424a383b52d121e523bab
pol-francesch <pfrances@purdue.edu>
2021-09-17 10:17:49 -0400

    worked on gettting better heat map and applying land mask

--- python_src/process_seho_out.py
+++ python_src/process_seho_out.py
@@ -1,8 +1,10 @@
+from branca import colormap
 import numpy as np
 import pandas as pd
 import folium
 from folium.plugins import HeatMap
 import branca
+from shapely.geometry import Polygon
 
 from os import listdir
 from os.path import isfile,join
@@ -21,7 +23,7 @@ def get_all_files_dir(path_to_dir: str) -> List[str]:
             path_to_dir (str): full path to directory
     '''
     file_names = [join(path_to_dir,f) for f in listdir(path_to_dir) if isfile(join(path_to_dir, f))]
-
+    
     return file_names
 
 def get_files_pd(file_names: List[str]) -> pd.DataFrame:
@@ -35,10 +37,9 @@ def get_files_pd(file_names: List[str]) -> pd.DataFrame:
                       'LatRx', 'LonRx', 'AltRx',
                       'TxID', 'LatTx', 'LonTx', 'AltTx',
                       'LatSp', 'LonSp', 'AltSp',
-                      'VSM', 'Sand', 'Clay', 'BulkDen', 'R_model', 'R_err', 'idk']
+                      'LandMask', 'idk']
     unused_columns = ['LatRx', 'LonRx', 'AltRx',
-                      'TxID', 'LatTx', 'LonTx', 'AltTx',
-                      'VSM', 'Sand', 'Clay', 'BulkDen', 'R_model', 'R_err', 'idk']
+                      'TxID', 'LatTx', 'LonTx', 'AltTx', 'idk']
     list_dfs = []
 
     for file_name in file_names:
@@ -50,19 +51,14 @@ def get_files_pd(file_names: List[str]) -> pd.DataFrame:
 
     df = pd.concat(list_dfs)
 
+    # Remove water samples (land=1, water=0)
+    df = df[df.LandMask == 1]
+
     return df
 
-def data_test(df):
-    df = df[df.LatSp > 40]
-    df = df[df.LatSp < 41]
-    df = df[df.LonSp > 30]
-    df = df[df.LonSp < 31]
-
-    print(df.info())
-
-def get_good_heatmap(specular_df):
+def get_specular_heatmap(specular_df):
     ''''
-        Trying to get revisit data from seho is a pain
+        Heatmap of the specular points
     '''
     # Group the measurements into buckets
     # Round lat and long and then use groupby to throw them all in similar buckets
@@ -77,15 +73,17 @@ def get_good_heatmap(specular_df):
     max_amt = float(df.countSp.max())
     print(max_amt)
 
+    # Generate Polygons
+    df['geometry'] = df.apply(lambda row: Polygon([(row.longitude-0.05, row.latitude-0.05), 
+                                                   (row.longitude+0.05, row.latitude-0.05),
+                                                   (row.longitude+0.05, row.latitude+0.05),
+                                                   (row.longitude-0.05, row.latitude+0.05)]), axis=1)
+    print(df.head())
     # Heat map
     hmap = folium.Map(location=[42.5, -80], zoom_start=7, )
     hm_wide = HeatMap(list(zip(df.latitude.values, df.longitude.values, df.countSp.values)),
                       gradient={0.0: '#00ae53', 0.2: '#86dc76', 0.4: '#daf8aa',
                                 0.6: '#ffe6a4', 0.8: '#ff9a61', 1.0: '#ee0028'},
-                                # min_opacity=0.5,
-                                # blur=50,
-                                # radius=
-                                
                                 )
     hmap.add_child(hm_wide)
 
@@ -94,10 +92,28 @@ def get_good_heatmap(specular_df):
                        '#ffe6a4', '#ff9a61', '#ee0028'],
                vmin=0,
                vmax=max_amt,
-               index=[0, 2, 4, 6, 8, 10])
+               index=[0, 4, 8, 12, 16, 20])
     # colormap = colormap.to_step(index=[0,2, 4, 6, 8, 10, 12])
     colormap.caption='Number of Specular Points'
     colormap.add_to(hmap)
+    # colormap_dept = branca.colormap.StepColormap(
+    #     colors=['#00ae53', '#86dc76', '#daf8aa',
+    #         '#ffe6a4', '#ff9a61', '#ee0028'],
+    #     vmin=0,
+    #     vmax=max_amt,
+    #     index=[0, 2, 4, 6, 8, 10, 12])
+    
+    # style_func = lambda x: {
+    #     'fillColor': colormap_dept(x['countSp']),
+    #     'color': '',
+    #     'weight': 0.0001,
+    #     'fillOpacity': 0.1
+    # }
+
+    # folium.GeoJson(
+    #     df,
+    #     style_function=style_func,
+    # ).add_to(hmap)
 
     hmap.save('test.html')
 
@@ -107,8 +123,8 @@ def get_revisit_info(specular_df, sim_start, sim_end):
     '''
     # Group the measurements into buckets
     # Round lat and long and then use groupby to throw them all in similar buckets
-    specular_df['approx_LatSp'] = round(specular_df['LatSp'], 1)
-    specular_df['approx_LonSp'] = round(specular_df['LonSp'], 1)
+    specular_df['approx_LatSp'] = round(specular_df['LatSp'],1)
+    specular_df['approx_LonSp'] = round(specular_df['LonSp'],1)
 
     grouped = specular_df.groupby(['approx_LatSp', 'approx_LonSp'])
 
@@ -124,13 +140,15 @@ def get_revisit_info(specular_df, sim_start, sim_end):
     
     return buckets
 
-def plot_revisit(buckets):
+def plot_revisit_heatmap(buckets):
     # Heat map
     hmap = folium.Map(location=[42.5, -80], zoom_start=7, )
     hm_wide = HeatMap(list(zip(buckets.bucket_LatSp.values, buckets.bucket_LonSp.values, buckets.MaxRevisit.values)),
                       gradient={0.0: '#00ae53', 0.2: '#86dc76', 0.4: '#daf8aa',
                                 0.6: '#ffe6a4', 0.8: '#ff9a61', 1.0: '#ee0028'})
     hmap.add_child(hm_wide)
+
+    print(max(buckets.MaxRevisit.values))
 
     # colormap = branca.colormap.StepColormap(
     #            colors=['#00ae53', '#86dc76', '#daf8aa',
@@ -142,15 +160,13 @@ def plot_revisit(buckets):
     # colormap.caption='Number of Specular Points'
     # colormap.add_to(hmap)
 
-    hmap.save('test.html')
+    hmap.save('test_revisit.html')
 
 if __name__ == "__main__":
     # This path assumes all files in the folder are for this test
-    path_to='/home/polfr/Documents/aae450-moist/test_output/out/'
+    path_to='/home/polfr/Downloads/sample/out/'
     file_names = get_all_files_dir(path_to)
     specular_df = get_files_pd(file_names)
-    # data_test(specular_df)
-    # generate_pass_heatmap(specular_df)
-    get_good_heatmap(specular_df)
+    get_specular_heatmap(specular_df)
     # buckets = get_revisit_info(specular_df, sim_start=2459580.50000, sim_end=2459580.62500)
-    # plot_revisit(buckets)
+    # plot_revisit_heatmap(buckets)
