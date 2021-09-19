4fcbbfc0f23138cca61e93b163c2a5e2f2c2dee2
pol-francesch <pfrances@purdue.edu>
2021-09-19 19:52:11 -0400

    updated revisit code so it is running much faster. can now plot revisit time!

--- python_src/process_seho_out.py
+++ python_src/process_seho_out.py
@@ -1,10 +1,12 @@
 from branca import colormap
 import numpy as np
+from numpy.core.numeric import NaN
 import pandas as pd
 import folium
 from folium.plugins import HeatMap
 import branca
 from shapely.geometry import Polygon
+import geopandas as gpd
 
 from os import listdir, walk
 from os.path import isfile,join
@@ -131,52 +133,113 @@ def get_revisit_info(specular_df, sim_start, sim_end):
     '''
         Returns array with the revisit info
     '''
-    # Group the measurements into buckets
     # Round lat and long and then use groupby to throw them all in similar buckets
     specular_df['approx_LatSp'] = round(specular_df['LatSp'],1)
     specular_df['approx_LonSp'] = round(specular_df['LonSp'],1)
 
-    grouped = specular_df.groupby(['approx_LatSp', 'approx_LonSp'])
+    # Calculate time difference
+    specular_df.sort_values(by=['approx_LatSp', 'approx_LonSp', 'JulianDay'], inplace=True)
+    specular_df['revisit'] = specular_df['JulianDay'].diff()
 
-    buckets = pd.DataFrame(columns=['bucket_LatSp', 'bucket_LonSp', 'SpPoints', 'Revisits', 'MaxRevisit'])
+    # Correct for borders
+    specular_df['revisit'].mask(specular_df.approx_LatSp != specular_df.approx_LatSp.shift(1), other=np.nan, inplace=True)
+    specular_df['revisit'].mask(specular_df.approx_LonSp != specular_df.approx_LonSp.shift(1), other=np.nan, inplace=True)
 
-    for key in tqdm(list(grouped.groups.keys())):
-        group = grouped.get_group(key)
-        times = group['JulianDay'].tolist()
-        times = sorted(times + [sim_end, sim_start])
-        diff = [t - s for s, t in zip(times, times[1:])]
-        temp = {'bucket_LatSp': key[0], 'bucket_LonSp': key[1], 'SpPoints': group.to_dict(), 'Revisits': diff, 'MaxRevisit': max(diff)}
-        buckets = buckets.append(temp, ignore_index=True)
-    
-    return buckets
+    # Get max revisit and store in new DF
+    indeces = specular_df.groupby(['approx_LatSp', 'approx_LonSp'])['revisit'].transform(max) == specular_df['revisit']
 
-def plot_revisit_heatmap(buckets):
+    max_rev_area_df = specular_df[indeces]
+
+    # Get rid of extra columns
+    # Any revisit that is less than 1 hour is remove. Typically this occurs because of a lack of samples (due to low sim time)
+    extra_cols = ['JulianDay', 'LatSp', 'LonSp', 'AltSp', 'LandMask']
+    max_rev_area_df['revisit'].mask(max_rev_area_df['revisit'] < 0.04, other=np.nan, inplace=True)
+    max_rev_area_df.drop(extra_cols, inplace=True, axis=1)
+
+    return max_rev_area_df
+
+def plot_revisit_heatmap(max_rev_area_df):
+    # Remove NaNs
+    max_rev_area_df = max_rev_area_df[max_rev_area_df['revisit'].notnull()]
+
     # Heat map
     hmap = folium.Map(location=[42.5, -80], zoom_start=7, )
-    hm_wide = HeatMap(list(zip(buckets.bucket_LatSp.values, buckets.bucket_LonSp.values, buckets.MaxRevisit.values)),
+    hm_wide = HeatMap(list(zip(max_rev_area_df.approx_LatSp.values, max_rev_area_df.approx_LonSp.values, max_rev_area_df.revisit.values)),
                       gradient={0.0: '#00ae53', 0.2: '#86dc76', 0.4: '#daf8aa',
                                 0.6: '#ffe6a4', 0.8: '#ff9a61', 1.0: '#ee0028'})
     hmap.add_child(hm_wide)
 
-    print(max(buckets.MaxRevisit.values))
+    max_amt = max(max_rev_area_df.revisit.values)
 
-    # colormap = branca.colormap.StepColormap(
-    #            colors=['#00ae53', '#86dc76', '#daf8aa',
-    #                    '#ffe6a4', '#ff9a61', '#ee0028'],
-    #            vmin=0,
-    #            vmax=max_amt,
-    #            index=[0, 2, 4, 6, 8, 10])
-    # colormap = colormap.to_step(index=[0,2, 4, 6, 8, 10, 12])
-    # colormap.caption='Number of Specular Points'
-    # colormap.add_to(hmap)
+    print(max_amt)
 
-    hmap.save('test_revisit.html')
+    colormap = branca.colormap.StepColormap(
+               colors=['#00ae53', '#86dc76', '#daf8aa',
+                       '#ffe6a4', '#ff9a61', '#ee0028'],
+               vmin=0,
+               vmax=max_amt,
+               index=[0, 2, 4, 6, 8, 10])
+    colormap.caption='Revisit Time'
+    colormap.add_to(hmap)
+
+    hmap.save('test_revisit_10day.html')
+
+def plot_revisit_heatmap_2(max_rev_area_df):
+    # First reduce the resolution of the polymap to avoid murdering my computer
+    # Round lat and long and then use groupby to throw them all in similar buckets
+    max_rev_area_df['approx_LatSp'] = round(max_rev_area_df['approx_LatSp'])
+    max_rev_area_df['approx_LonSp'] = round(max_rev_area_df['approx_LonSp'])
+
+    # Get max revisit and store in new DF
+    indeces = max_rev_area_df.groupby(['approx_LatSp', 'approx_LonSp'])['revisit'].transform(max) == max_rev_area_df['revisit']
+
+    max_rev_area_df = max_rev_area_df[indeces]
+
+    # Now generate the map
+    map = folium.Map(location=[42.5, -80], zoom_start=7, )
+
+    # Remove NaNs
+    max_rev_area_df = max_rev_area_df[max_rev_area_df['revisit'].notnull()]
+    # Generate Polygons
+    max_rev_area_df['geometry'] = max_rev_area_df.apply(lambda row: Polygon([(row.approx_LonSp-0.5, row.approx_LatSp-0.5), 
+                                                                             (row.approx_LonSp+0.5, row.approx_LatSp-0.5),
+                                                                             (row.approx_LonSp+0.5, row.approx_LatSp+0.5),
+                                                                             (row.approx_LonSp-0.5, row.approx_LatSp+0.5)]), axis=1)
+    max_amt = max(max_rev_area_df.revisit.values)
+    print(max_amt)
+    print(max_rev_area_df)
+    colormap_dept = branca.colormap.StepColormap(
+        colors=['#00ae53', '#86dc76', '#daf8aa',
+            '#ffe6a4', '#ff9a61', '#ee0028'],
+        vmin=0,
+        vmax=max_amt,
+        index=[0, 2, 4, 6, 8, 10])
+
+    for _, r in tqdm(max_rev_area_df.iterrows(), total=max_rev_area_df.shape[0]):
+        style_func = lambda x: {
+            'fillColor': colormap_dept(r['revisit']),
+            'color': '',
+            'weight': 1.0,
+            'fillOpacity': 0.5
+        }
+        sim_geo = gpd.GeoSeries(r['geometry'])
+        geo_j = sim_geo.to_json()
+        geo_j = folium.GeoJson(data=geo_j, style_function=style_func, overlay=True, control=True)
+        geo_j.add_to(map)
+    
+    # Add legend
+    colormap_dept.caption='Revisit Time'
+    colormap_dept.add_to(map)
+    # map.add_child(folium.LayerControl())
+
+    # Save it
+    map.save('revisit_polymap_10day.html')
 
 if __name__ == "__main__":
     # This path assumes all files in the folder are for this test
     path_to='/home/polfr/Documents/dummy_data/09_17_2021/Unzipped/'
     file_names = get_all_files_dir(path_to)
     specular_df = get_files_pd(file_names)
-    get_specular_heatmap(specular_df)
-    # buckets = get_revisit_info(specular_df, sim_start=2459580.50000, sim_end=2459580.62500)
-    # plot_revisit_heatmap(buckets)
+    # get_specular_heatmap(specular_df)
+    max_rev_area_df = get_revisit_info(specular_df, sim_start=2459580.50000, sim_end=2459580.62500)
+    plot_revisit_heatmap_2(max_rev_area_df)
