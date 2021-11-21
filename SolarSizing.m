ee6c9fe8817166e1bade2532a33978df5102b2ea
mberthin <79489233+mberthin@users.noreply.github.com>
2021-11-21 18:03:06 -0500

    Update SolarSizing.m

--- SolarSizing.m
+++ SolarSizing.m
@@ -3,7 +3,7 @@
 
 %% Notes From Research 
 %(SOA_Power)
-%COTS operating tmep -40 to 85 C
+%COTS operating temp -40 to 85 C
 %BOL efficiencies p.27, ranges from 16-31%
 %BOL Peak SA Power values on p.29
 %Battery producttable 3-3 p.35
@@ -19,7 +19,7 @@
 %sunlight angle and power generated, losses only due to efficiency)
 %peak power tracking requiires power converter btw array and laods
 %P_o for given junctions: Si = 202, GaAs = 253, Multi = 383
-%performance degradation per yr (%): Si = 3.75, GaAs = 2.75, Tri[ple jun = 0.5
+%performance degradation per yr (%): Si = 3.75, GaAs = 2.75, Triple jun = 0.5
 %triple junction GaAs high efficiency with high cost, silicon is lower cost where radtiation not concern
 %table 21-17 for primary batteries (short missions and long term tasks like
 %battery backup)
@@ -38,23 +38,23 @@
 
 %% Inputs
 P_d = 61; %daytime power load [W], how much power system uses
-T_d = ; %time of orbit spent in daylight [s]
+T_d = 2868; %time of orbit spent in daylight [s]
 P_e = P_d; %eclipse power load [W]
-T_e = ; %time of orbit spent in eclipse [s]
+T_e = 2868; %time of orbit spent in eclipse [s]
 X_d = 0.80; %efficiency of getting power from SA directly to loads
 X_e = 0.6; %Efficiency of getting power from SA to batteries then laods
-P_i = ; %Input solar power density [W/m^2], different at each orbit
+P_i = 1367; %Input solar power density [W/m^2], different at each orbit
 n = 0.29; %solar cell efficiency
-I_d = 0.185; %degradation of solar sell for GaAs (SJ)
+I_d = 0.72; %inherent degradation of solar cell, nominal value in SMAD table 21-14
 theta = 0; %angle btw solar cell normal and the sun, use worst case (23.5 inSMAD FiresatII)
 
 %% Calculations
 
 P_SA = 1 / T_d * ((P_d * T_d / X_d) + (P_e * T_e) / X_e); %Required solar array output power [W]
-P_o = P_i * n; %Output solar power desnity [W/m^2]
-P_bol = P_o * I_d * cos(theta); %beginning of life power output density [W/m^2]
+P_o = P_i * n; %Output solar power density [W/m^2]
+P_bol = P_o * I_d * cosd(theta); %beginning of life power output density [W/m^2]
+L_d = (1 - 0.0275) ^ 3; %lifetime degradation of solar cells, 3 is mission lifetime in years
 P_eol = P_bol * L_d; %end of life power output density [W/m^2]
-L_d = (1 - 0.0275) ^ 3; %lifetime degration of solar cells, 3 is mission lifetime in years
-A_SA = P_SA / P_EOL; %required solar panel area [m^2]
+A_SA = P_SA / P_eol; %required solar panel area [m^2]
 
-%calcualte mass as well
+m_SA = A_SA * 2.8; %estimated mass of array (use 2.8 kg/m^2 for MJ GaAs) [kg]
